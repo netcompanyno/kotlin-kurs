9b9962e8baf872987d2837b4932e21f8b20baf6d
Philip Holt-Seeland <philipbakken.holt-seeland@drift.oslo.kommune.no>
2019-09-28 19:57:40 +0200

    web security config

---
+++ src/main/kotlin/com/netcompany/characters/config/WebSecurityConfig.kt
@@ -0,0 +1,11 @@
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.servlet.config.annotation.CorsRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+
+@Configuration
+open class WebSecurityConfig : WebMvcConfigurer {
+    override fun addCorsMappings(registry: CorsRegistry?) {
+        registry!!
+            .addMapping("/**").allowedMethods("*")
+    }
+}
