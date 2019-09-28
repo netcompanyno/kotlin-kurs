131c14d6e9f08f1985cb1c48d272a2e7deaeb519
marstran <marstran90@gmail.com>
2019-09-28 17:01:07 +0200

    Byttet om på oppgave 2 og 3

--- src/main/kotlin/Oppgave3.kt
+++ src/main/kotlin/Oppgave3.kt
@@ -1,29 +1,23 @@
 /**
  * Oppgave 3
  *
- * I denne oppgaven vil du bli litt mer kjent med hvordan null-safety og smart casting
- * fungerer i Kotlin.
- * Skriv funksjonen handleRemoteCall etter følgende krav, og få testene i Oppgave3Test til å kjøre grønt.
- *
- * - Dersom id er null, returner en respons med status 404.
- * - Dersom id ikke er null, kall callRemoteAPI
- * - Dersom callRemoteAPI returnerer null, returner en respons med status 404.
- * - Dersom callRemoteAPI returnerer en respons med status 500, kast en exception.
- * - Ellers, returner responsen fra callRemoteAPI.
+ * Å nei! Koden nedenfor kompilerer ikke.
+ * Gjør det som skal til for at koden kompilerer, ola + kari == sant blir true, og at testene i "Oppgave2Test" kjører grønt.
  */
 
-fun handleRemoteCall(id: Int?): Response = TODO()
+class Person(???)
+class Couple(???)
 
+// En operator function som heter 'plus' kan kalles med +.
+// Du kan lese mer om operator functions her: https://kotlinlang.org/docs/reference/operator-overloading.html
+operator fun ???.plus(other: ???): ??? = TODO()
 
-// Ikke endre dette --------------
-data class Response(val status: Int)
+fun main() {
 
-var ignoreThis = 0
-fun callRemoteAPI(id: Int): Response? {
-    ignoreThis++
-    return when {
-        id % 3 == 0 -> Response(200)
-        id % 3 == 1 -> Response(500)
-        else -> null
-    }
+    val ola = Person(name = "Ola")
+    val kari = Person(name = "Kari")
+    val sant = Couple(ola, kari)
+
+    println(ola + kari == sant)
+
 }
--- src/test/kotlin/Oppgave2Test.kt
+++ src/test/kotlin/Oppgave2Test.kt
@@ -1,27 +1,40 @@
-import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
+import kotlin.test.Test
 import kotlin.test.assertEquals
 
 class Oppgave2Test {
 
     @Test
-    fun `Person("Ola") skal ha name = Ola`() {
-        val ola = Person("Ola")
-
-        assertEquals("Ola", ola.name)
+    fun `Dersom id er null, returner en respons med status 404`() {
+        val response = handleRemoteCall(null)
+        assertEquals(Response(404), response)
     }
 
     @Test
-    fun `Person("Ola") skal være lik Person("Ola")`() {
-        assertEquals(Person("Ola"), Person("Ola"))
+    fun `Dersom id er ikke er null, kall callRemoteAPI`() {
+        val before = ignoreThis
+        handleRemoteCall(5)
+        val after = ignoreThis
+        assertEquals(after, before + 1)
     }
 
     @Test
-    fun `ola + kari == sant skal gi true`() {
-        val ola = Person("Ola")
-        val kari = Person("Kari")
-        val sant = Couple(ola, kari)
+    fun `Dersom callRemoteAPI returnerer null, returner en respons med status 404`() {
+        val response = handleRemoteCall(2)
+        assertEquals(Response(404), response)
+    }
 
-        assertEquals(ola + kari, sant)
+    @Test
+    fun `Dersom callRemoteAPI returnerer en respons med status 500, kast en exception`() {
+        assertThrows<Exception> {
+            handleRemoteCall(1)
+        }
+    }
+
+    @Test
+    fun `Ellers, returner responsen fra callRemoteAPI`() {
+        val response = handleRemoteCall(3)
+        assertEquals(Response(200), response)
     }
 
 }
