91d5f87ee41a35b3c854e55c72306b7d7a709b3d
marstran <marstran90@gmail.com>
2019-09-28 16:58:33 +0200

    Byttet om på oppgave 2 og 3

--- src/main/kotlin/Oppgave2.kt
+++ src/main/kotlin/Oppgave2.kt
@@ -1,23 +1,29 @@
 /**
  * Oppgave 2
  *
- * Å nei! Koden nedenfor kompilerer ikke.
- * Gjør det som skal til for at koden kompilerer, ola + kari == sant blir true, og at testene i "Oppgave2Test" kjører grønt.
+ * I denne oppgaven vil du bli litt mer kjent med hvordan null-safety og smart casting
+ * fungerer i Kotlin.
+ * Skriv funksjonen handleRemoteCall etter følgende krav, og få testene i Oppgave3Test til å kjøre grønt.
+ *
+ * - Dersom id er null, returner en respons med status 404.
+ * - Dersom id ikke er null, kall callRemoteAPI
+ * - Dersom callRemoteAPI returnerer null, returner en respons med status 404.
+ * - Dersom callRemoteAPI returnerer en respons med status 500, kast en exception.
+ * - Ellers, returner responsen fra callRemoteAPI.
  */
 
-class Person(???)
-class Couple(???)
+fun handleRemoteCall(id: Int?): Response = TODO()
 
-// En operator function som heter 'plus' kan kalles med +.
-// Du kan lese mer om operator functions her: https://kotlinlang.org/docs/reference/operator-overloading.html
-operator fun ???.plus(other: ???): ??? = TODO()
 
-fun main() {
+// Ikke endre dette --------------
+data class Response(val status: Int)
 
-    val ola = Person(name = "Ola")
-    val kari = Person(name = "Kari")
-    val sant = Couple(ola, kari)
-
-    println(ola + kari == sant)
-
+var ignoreThis = 0
+fun callRemoteAPI(id: Int): Response? {
+    ignoreThis++
+    return when {
+        id % 3 == 0 -> Response(200)
+        id % 3 == 1 -> Response(500)
+        else -> null
+    }
 }
