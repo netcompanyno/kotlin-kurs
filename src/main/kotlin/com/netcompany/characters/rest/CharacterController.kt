316fea54b23fdd98e0725ec89ceb3ae592f6bad0
Philip Holt-Seeland <philipbakken.holt-seeland@drift.oslo.kommune.no>
2019-10-01 16:13:14 +0200

    controller

--- src/main/kotlin/com/netcompany/characters/rest/CharacterController.kt
+++ src/main/kotlin/com/netcompany/characters/rest/CharacterController.kt
@@ -2,10 +2,10 @@ package com.netcompany.characters.rest
 
 import com.netcompany.characters.dto.CharacterDto
 import com.netcompany.characters.service.CharacterService
-import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
 
-@Controller
+@RestController
 class CharacterController(val characterService: CharacterService) {
     @GetMapping(path = ["/hello"])
     fun hello() = "Hello, Yoda!"
