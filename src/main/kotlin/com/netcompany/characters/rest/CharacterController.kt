30b0162535fd4839b33dda902f6827ccfc25de97
Philip Holt-Seeland <philipbakken.holt-seeland@drift.oslo.kommune.no>
2019-10-20 23:24:24 +0200

    exercise  5

--- src/main/kotlin/com/netcompany/characters/rest/CharacterController.kt
+++ src/main/kotlin/com/netcompany/characters/rest/CharacterController.kt
@@ -5,6 +5,15 @@ import com.netcompany.characters.service.CharacterService
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 
+/**
+ * Opgpave 5
+ *
+ * Lag et endepunkt for å opprette karakterer. Dette skal ta imot en karakter, som skal sendes gjennom servicelaget
+ * og lagres i databasen. Endepunktet skal retunere den nylig opprettede karakteren.
+ *
+ * Når du er ferdig skal man kunne opprette en ny karakter fra frontend, og ved å trykke på "Refresh"-knappen skal denne
+ * dukke opp i listen. Du kan også kjøre testene for å se om du har løst oppgaven riktig.
+ */
 @RestController
 class CharacterController(val characterService: CharacterService) {
     @GetMapping(path = ["/hello"])
