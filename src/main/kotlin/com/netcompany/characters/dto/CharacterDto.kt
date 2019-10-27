272fd0fa82bdc8899ab99d7826b47acefbb60f47
Philip Holt-Seeland <philipbakken.holt-seeland@drift.oslo.kommune.no>
2019-10-27 14:22:06 +0100

    var -> val

--- src/main/kotlin/com/netcompany/characters/dto/CharacterDto.kt
+++ src/main/kotlin/com/netcompany/characters/dto/CharacterDto.kt
@@ -2,6 +2,6 @@ package com.netcompany.characters.dto
 
 import com.netcompany.characters.domain.CharacterEntity
 
-data class CharacterDto(var name: String, var height: Int, var homeworld: String){
+data class CharacterDto(val name: String, val height: Int, val homeworld: String){
     constructor(entity: CharacterEntity): this(entity.name, entity.height, entity.homeworld)
 }
