026bdd1a361928dd8b400f5e04b0c92aa173928c
marstran <marstran90@gmail.com>
2019-10-29 18:23:13 +0100

    Fikset exception mapping for require i init.

--- src/main/kotlin/com/netcompany/characters/exception/ExceptionHandler.kt
+++ src/main/kotlin/com/netcompany/characters/exception/ExceptionHandler.kt
@@ -1,24 +1,36 @@
 package com.netcompany.characters.exception
 
+import com.fasterxml.jackson.databind.exc.InvalidDefinitionException
 import org.springframework.http.HttpStatus
+import org.springframework.http.MediaType
 import org.springframework.http.ResponseEntity
-import org.springframework.http.converter.HttpMessageConversionException
 import org.springframework.web.bind.annotation.ControllerAdvice
 import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler
 
 @ControllerAdvice
 class ExceptionHandler : ResponseEntityExceptionHandler() {
-    @ExceptionHandler(value = [Exception::class])
-    fun handleException(exception: Exception): ResponseEntity<String> {
-        if (exception is CharacterNotFoundException) {
-            throw exception
+
+    @ExceptionHandler(value = [InvalidDefinitionException::class])
+    fun handleException(exception: InvalidDefinitionException): ResponseEntity<String> {
+        val cause = exception.cause
+
+        // Dette er en liten hack for å få require i init til å fungere.
+        // Det finnes sannsynligvis en bedre måte å løse dette på.
+        if (cause is IllegalArgumentException) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(
+                    """
+                {
+                  "code": 500, 
+                  "message": "${cause.message}"
+                }
+            """.trimIndent()
+                )
         }
-        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(exception.message)
+
+        throw exception
     }
 
-    @ExceptionHandler(value = [HttpMessageConversionException::class])
-    fun handleHttpMessageConversionException(exception: HttpMessageConversionException): ResponseEntity<String> {
-        return ResponseEntity.badRequest().body(exception.message)
-    }
 }
