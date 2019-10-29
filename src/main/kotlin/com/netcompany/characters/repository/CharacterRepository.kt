6f6bac542e90dbb0431943a7f2999aa941d9fbc6
Philip Holt-Seeland <philipbakken.holt-seeland@drift.oslo.kommune.no>
2019-10-29 22:10:41 +0100

    update exercise wording.

--- src/main/kotlin/com/netcompany/characters/repository/CharacterRepository.kt
+++ src/main/kotlin/com/netcompany/characters/repository/CharacterRepository.kt
@@ -5,5 +5,7 @@ package com.netcompany.characters.repository
  *
  * Fullfør initialiseringen av dette interfacet for å gjøre det mulig å håndtere databaseoperasjoner for characters.
  * Entitetsobjektet er CharacterEntity, og id skal være av typen Int.
+ *
+ * Tips: en metode kalt findAll vil returnere alle forekomstene av en entitet.
  */
 interface CharacterRepository
